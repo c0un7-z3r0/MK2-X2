25fa1b6464b8d5b87ac281f2c6c6e649ca80b817
David Kagerer <david@binder-kagerer.de>
2017-07-09 00:32:23 +0200

    Update install-new-firmware.sh

--- install-new-firmware.sh
+++ install-new-firmware.sh
@@ -4,12 +4,13 @@ git submodule update
 
 # copy configs into the right folder
 cd Firmware/Marlin/Marlin
+git pull origin printers/default
+
 rm -rf Configuration.h
 rm -rf Configuration_adv.h
 cp my_configuration_files/MK2-X2/Configuration.h .
 cp my_configuration_files/MK2-X2/Configuration_adv.h .
 cd ..
-git pull origin printers/default
 
 # compile and upload
 pio run -t upload
