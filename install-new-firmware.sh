2195f61080fcc7fab2af72d3a5fb7c89717cfbd2
David Kagerer <david@binder-kagerer.de>
2018-04-18 23:01:10 +0200

    Update install-new-firmware.sh

--- install-new-firmware.sh
+++ install-new-firmware.sh
@@ -9,8 +9,10 @@ git pull origin printers/default
 
 rm -rf Configuration.h
 rm -rf Configuration_adv.h
+rm -rf pins_RAMPS.h
 cp my_configuration_files/MK2-X2/Configuration.h .
 cp my_configuration_files/MK2-X2/Configuration_adv.h .
+cp my_configuration_files/MK2-X2/pins_RAMPS.h .
 cd ..
 
 # compile and upload
